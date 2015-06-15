ff851f261327a0b0735c5348366417813b512937
Abhinav Sinha <abhinav.sinha@fissionlabs.in>
2015-06-15 08:26:15 +0000

    Parse_insert_cass.py edited online by Abhinav Sinha

--- NASA logs/Parse_insert_cass.py
+++ NASA logs/Parse_insert_cass.py
@@ -6,23 +6,34 @@ from pycassa.pool import ConnectionPool
 from pycassa.columnfamily import ColumnFamily
 from pycassa.system_manager import *
 
-def initialize_connection(file_path):
+def create_keyspace(key_space, column_fam):
 
+    sys = SystemManager('localhost:9160')
+    sys.create_keyspace(key_space, SIMPLE_STRATEGY, {'replication_factor': '1'})
+    validators = {'host': UTF8_TYPE,'timestamp': UTF8_TYPE, 'requestline': UTF8_TYPE, 'statuscode': UTF8_TYPE, 'bytesize': UTF8_TYPE}
+    sys.create_column_family(key_space, column_fam, super=False, comparator_type=UTF8_TYPE, key_validation_class=UTF8_TYPE, column_validation_classes=validators)
+    sys.close()
+    return 1
+
+def initialize_connection(key_space, column_fam):
+
+    file_path = "/home/abhinav/Downloads/NASA_Aug95"
     uuid = 100000
+    cass_client = ConnectionPool(key_space, ['localhost:9160'])
+    col_fam = ColumnFamily(cass_client, column_fam)
     # function call
     extract_data(file_path, uuid)
     print "Added data to the Cassandra database"
-    print "Total time elapsed: %s" % (datetime.now() - t_init)
     return 1
 
 def extract_data(file_path, num):
+
     row_count = 0
     text = open(file_path).readlines()
-    # log_format: in24.inetnebr.com - - [01/Aug/1995:00:00:01 -0400] "GET /shuttle/missions/sts-68/news/sts-68-mcc-05.txt HTTP/1.0" 200 1839
-    
-    #extracting the logdata in every line 
+    #extracting the logdata in every line
     for data in text:
         print "Row: %s" % row_count
+        # log_format: in24.inetnebr.com - - [01/Aug/1995:00:00:01 -0400] "GET /shuttle/missions/sts-68/news/sts-68-mcc-05.txt HTTP/1.0" 200 1839
         # host_data
         try:
             # searching for host_data in the line using re identifiers 
@@ -51,9 +62,9 @@ def extract_data(file_path, num):
 
         # status_code, byte_size
         try:
-            location = data.find('" ')
-            status = data[location+2:].split(" ")[0]
-            bytes = (data[location:].split(" "))[2].split('\n')[0]
+            loc = data.find('" ')
+            status = data[loc+2:].split(" ")[0]
+            bytes = (data[loc:].split(" "))[2].split('\n')[0]
             # to skip the lines with errors --> no space between statuscode and bytesize data
             if " " in status:
                 continue
@@ -63,7 +74,7 @@ def extract_data(file_path, num):
         # function call
         insert_data(num, host, date, requestline, status, bytes)
         num = num + 1
-        row_count = row_count + 1
+        counter = counter + 1
     return 1
 
 def insert_data(num, host, date, requestline, status, bytes):
@@ -76,17 +87,10 @@ if __name__ == '__main__':
 
     global col_fam, t_init
     t_init = datetime.now()
-    file_path = "/home/abhinav/Downloads/NASA_Aug95"
-    
+    key_space = 'main_keyspace'
+    column_fam = 'main_column'
     # creating Keyspace and ColumnFamily using pycassaShell commands
-    sys = SystemManager('localhost:9160')
-    sys.create_keyspace('main_keyspace', SIMPLE_STRATEGY, {'replication_factor': '1'})
-    validators = {'host': UTF8_TYPE,'timestamp': UTF8_TYPE, 'requestline': UTF8_TYPE, 'statuscode': UTF8_TYPE, 'bytesize': UTF8_TYPE}
-    sys.create_column_family('main_keyspace', 'main_column', super=False, comparator_type=UTF8_TYPE, key_validation_class=UTF8_TYPE, column_validation_classes=validators)
-    sys.close()
-
-    # connecting to the keyspace and columnfamily
-    cass_client = ConnectionPool('main_keyspace', ['localhost:9160'])
-    col_fam = ColumnFamily(cass_client, 'main_column')
+    create_keyspace(key_space, column_fam)
     # function call
-    initialize_connection(file_path) 
+    initialize_connection(key_space, column_fam)
+    print "Total time elapsed: %s" % (datetime.now() - t_init)

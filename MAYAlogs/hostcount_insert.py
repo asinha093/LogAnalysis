fe7af4c9d0c308c6fffc236187935628501ccd3c
Rahul Penti <rahul.penti@fissionlabs.in>
2015-06-26 15:43:37 +0530

    Initial edits done

--- MAYAlogs/hostcount_insert.py
+++ MAYAlogs/hostcount_insert.py
@@ -2,29 +2,30 @@ __author__ = 'rahul'
 
 from pycassa.pool import ConnectionPool
 from pycassa.columnfamily import ColumnFamily
-from cassandra.cluster import Cluster
+from cassandra.cluster import Cluster, BatchStatement
 from pyspark import SparkConf
 from pyspark_cassandra.context import *
-import multiprocessing
 import datetime
-global col_fam,session
-pool = ConnectionPool('main',['localhost:9160'])
-col_fam = ColumnFamily(pool,'random2')
-cluster = Cluster()
-session = cluster.connect('main')
+import uuid
 
 
-def insert_cassandra(uid, host, links, user_agent, byte_transfer, response_time):
-    qwe = session.execute("""INSERT INTO ipdata (uid, host, links, user_agent, byte_transfer, response_time) VALUES (%s, %s, %s, %s, %s, %s)""",(uid, host, links, user_agent, byte_transfer, response_time))
+def batch_prepare(batch, fields,dest,session):
+    batch_statement = session.prepare("INSERT INTO " +dest+"(uid, host, links, user_agent, byte_transfer, response_time) VALUES (?, ?, ?, ?, ?, ?)")
+    batch.add(batch_statement, [uuid.uuid1(), fields[0], fields[1], fields[2], fields[3], fields[4]])
+    return batch
+
+def insert_fields(batch,session):
+    session.execute(batch)
     return 1
 
-def retrv_stats(keys, host, num):
+
+def retrv_stats(keys, host,source_conn):
     links, vir_mem ,reqtype, userdata = [], [], [], []
     bytes, avg_time, count, unique_visits = 0, 0, 0, 0
     temp1 = []
     for x in keys:
         temp1.append(str(x))
-    data = col_fam.multiget(temp1)
+    data = source_conn.multiget(temp1)
     for i in data.values():
         links.append(i["reqtlink"])
         userdata.append(i['user_data'])
@@ -34,7 +35,7 @@ def retrv_stats(keys, host, num):
             avg_time += int(i['response_time'])
             count += 1
     avg_time = avg_time/count
-    return insert_cassandra(num, host, links, userdata, bytes, avg_time)
+    return host, links, userdata, bytes, avg_time
 
 def unique_count(set):
     temp = []
@@ -48,31 +49,31 @@ def unique_count(set):
     return temp, temp2
 
 
-def initilize_conn():
+def initilize_conn(cluster, keyspace,source,dest):
+    pool = ConnectionPool(keyspace,[cluster])
+    col_fam = ColumnFamily(pool,source)
+    cluster = Cluster()
+    session = cluster.connect(keyspace)
     conf = SparkConf().set("spark.cassandra.connection.host", "127.0.0.1").set("spark.cassandra.connection.native.port","9042")
     sc = CassandraSparkContext(conf=conf)
-    rdd = sc.cassandraTable("main", "random2").cache()
+    rdd = sc.cassandraTable(keyspace, source).cache()
     time_sort = rdd.select("host","key").groupByKey().collect()
-    total = 0
-    count = 0
-    num = 100000
-    t7 = datetime.datetime.now()
-    print len(time_sort)
+    batch = BatchStatement()
+    row_count=0
     for x in time_sort:
         t = datetime.datetime.now()
-        #if len(x[1]) > 5:
-        retrv_stats(x[1], x[0],num)
-        #else:
-        #    insert_cassandra(num,x[0],['-'],[0],['-'],[0],0,0)
-        total += len(x[1])
-        num += 1
-        count += 1
-        print total, count, len(time_sort)
-        print datetime.datetime.now()- t, datetime.datetime.now() - t7
+        fields = retrv_stats(x[1], x[0],col_fam)
+        batch = batch_prepare(batch, fields,dest,session)
+        if row_count % 1000 == 0:
+            insert_fields(batch,session)
+            batch = BatchStatement()
+        elif row_count == len(time_sort):
+            # inserting the final batch
+            insert_fields(batch,session)
 
-    print 'done'
+
 if __name__ == '__main__':
-    initilize_conn()
+    initilize_conn('localhost:9160','main',"parsed_data_cli","ipdata")
 
 
 
